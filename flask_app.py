f640890a20300dc7e4ba1371575b9033b369d175
José María Cruz Parada <ichema@MacBook-Pro.local>
2024-06-12 17:29:10 -0600

    [refactor]  crud users

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 # Construccion del aplicativo utilizando el Framework Flask de Python
 app = Flask(__name__)
@@ -53,3 +54,71 @@ def login():
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
+
+# Crear usuario
+@app.route('/create_user',methods=["POST"])
+def create_user():
+    # Obtener los datos
+    if request.is_json:
+        data = request.get_json()
+        name = data.get("name")
+        mail = data.get("email")
+        pwd = data.get("password")
+    else:
+        name = request.form.get("name")
+        mail = request.form.get("email")
+        pwd = request.form.get("password")
+
+    # Verificar si hay datos para agregar y
+    # crear el usuario en la base de datos
+    if name and mail and pwd:
+        user = User(name = name,email = mail)
+        user.set_password(pwd)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"Exito":f"Usuario {user.name} creado correctamente"})
+    else:
+        jsonify({"Error":"Datos incompletos"})
+
+@app.route ('/get_users', methods=["GET"])
+def get_users():
+    # Traer usuarios
+    users = User.query.filter_by(deleted_at = None).all()
+    result = [
+        {
+            "id":user.id,
+            "name":user.name,
+            "email":user.email,
+            "created_at":user.created_at
+        }
+        for user in users
+        ]
+    return jsonify(result)
+
+@app.route ('/get_user_by_id/<int:id>', methods=["GET"])
+def get_user_by_id(id):
+    # Traer usuario
+    # Cuando no se usa borrado lógico
+    #user = User.query.get_or_404(id)
+    user = User.query.filter_by(id = id,deleted_at = None).first_or_404()
+    result = {
+            "id":user.id,
+            "name":user.name,
+            "email":user.email,
+            "created_at":user.created_at
+        }
+    return jsonify(result)
+
+@app.route ('/delete_user/<int:id>', methods=["DELETE"])
+def delete_user(id):
+    user = User.query.get_or_404(id)
+    if user:
+        # Borrado lógico
+        user.deleted_at = datetime.now()
+        # Borrado físico
+        #db.session.delete(user)
+
+        db.session.commit()
+        return jsonify({"Exito":"Usuario eliminado"})
+    else:
+        return jsonify({"Error":"Usuario no encontrado"})
